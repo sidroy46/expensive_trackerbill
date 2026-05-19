f9320d6e71904c5e45f01f7881a347411b998556
sidroy46 <sathanapallysiddharth@gmail.com>
2026-05-19 10:41:21 +0530

    Increase Express body parser JSON payload limit to 50MB to support large receipt image saves

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,8 @@ const PORT = process.env.PORT || 5000;
 
 // Standard Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Create uploads directory if it doesn't exist (Only in local development, skip on Vercel!)
